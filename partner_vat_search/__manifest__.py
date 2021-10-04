9a7106be48bc99bd6508993cd92b5144e5b63462
ChristianSantamaria <christiancameselle@hotmail.com>
2021-10-04 17:13:49 +0200

    [ADD] partner_vat_search

---
+++ partner_vat_search/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Partner Vat Search",
+    "summary": """
+        Adds search by VAT without having to add it completely
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://github.com/solvosci/slv-partner",
+    "depends": ["contacts"],
+    "data": [
+        "views/res_partner_view.xml"
+    ],
+    'installable': True,
+}
