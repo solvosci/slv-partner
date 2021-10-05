cb3a77d51ee7a2d3f045d28404f7cb320b2ebabd
ChristianSantamaria <christiancameselle@hotmail.com>
2021-10-05 10:32:20 +0200

    [ADD] partner_ship

---
+++ partner_ship/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Partner Ship",
+    "summary": """
+        Adds new ship fields to contacts
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://github.com/solvosci/slv-partner",
+    "depends": ["contacts"],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    'installable': True,
+}
