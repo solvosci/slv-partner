2d573d3113c0c0c9f2168ff3f3502cec63385801
Dante Pereyra <dante.pereyra@solvos.es>
2024-11-29 10:27:53 +0100

    [ADD] partner_ref_views

---
+++ partner_ref_views/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Partner Ref Views",
+    "summary": """
+        Improves search & tree views adding Reference field
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://github.com/solvosci/slv-partner",
+    "depends":["base"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    'installable': True,
+}
