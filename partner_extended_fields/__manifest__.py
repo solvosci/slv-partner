9b320b209dddc877e714003b05809c7719ce5041
Iria Alonso <iria.alonso@solvos.es>
2025-05-07 10:26:36 +0200

    [ADD] partner_extended_fields New fields added to Partner model: - 'Has container'

---
+++ partner_extended_fields/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Partner Extended Fields",
+    "summary": """
+        New fields added to Partner model:
+        - 'Has container'
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://github.com/solvosci/slv-partner",
+    "data": [
+        "views/res_partner_views.xml"
+    ],
+    "installable": True,
+}
---
+++ partner_extended_fields/readme/ROADMAP.rst
@@ -0,0 +1,1 @@
+Create a new page for the new added fields.
