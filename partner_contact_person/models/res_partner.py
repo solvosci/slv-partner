24101080d525fcfc0ae564db17d34013ba8b8f39
Dante Pereyra <dante.pereyra@solvos.es>
2025-08-01 13:15:15 +0200

    [MIG] partner_contact_person: Migration to 17.0

--- partner_contact_person/models/res_partner.py
+++ partner_contact_person/models/res_partner.py
@@ -12,6 +12,7 @@ class ResPartner(models.Model):
         comodel_name="res.partner",
         help="This partner is the contact person for this company",
         domain="[('is_company','=',False)]",
+        copy=False,
     )
     contact_person_name = fields.Char(related="contact_person_id.name")
     contact_person_email = fields.Char(related="contact_person_id.email")
